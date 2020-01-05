afea43d272b410cccdc55b69036b0ff6a5bf895e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-05 08:05:52 +0000

    Deploy elu00/elu00.github.io to github.com/elu00/elu00.github.io.git:master

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,16 @@
+var store = [{
+        "title": "DIY 10 Channel PWM Controller - Build Log Part 0",
+        "excerpt":"Motivation Recently within the PC building space, the majority of fan vendors have come to support PWM Control, a standard used for fan and temperature management. Compared to DC control, PWM control is nice to have because it tends to provide a larger effective operating range and smoother speed adjustments...","categories": ["Projects"],
+        "tags": ["Controller-Build-Log"],
+        "url": "https://elu00.github.io/projects/Build-Log-Part-0/",
+        "teaser":null},{
+        "title": "HackCMU 2018: Viz.ml",
+        "excerpt":"About Last weekend, I participated in HackCMU 2018, a beginner-oriented hackathon hosted at CMU. Although I have mixed thoughts on the event itself, today I’ll be focusing on my project for it, viz.ml, which I worked on with my roommate, Max Slater. Largely based off of this blog post, our...","categories": ["Projects"],
+        "tags": [],
+        "url": "https://elu00.github.io/viz.ml/",
+        "teaser":null},{
+        "title": "Markov Chains and the Perron-Frobenius theorem (Part 1/2)",
+        "excerpt":"Disclaimer Much of the content of this post is based off content from 21-242 at CMU as taught by Clinton Conley. Consequently, I claim no credit for any of the ideas presented below, but (as explained below), I think that this approach in particular is worth sharing because of the...","categories": ["Math"],
+        "tags": ["linear-algebra","pagerank","Perron-Frobenius"],
+        "url": "https://elu00.github.io/math/markov-pt1/",
+        "teaser":null}]
